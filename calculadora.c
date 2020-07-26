65fa0aedc89ae53de0d56ae2ee0018eab1b4d41c
Maria Boaretto <mariaboarettocampos@gmail.com>
2020-07-26 12:30:52 -0300

    Menu com opções + msg de erro

--- calculadora.c
+++ calculadora.c
@@ -2,30 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 
-void menu(int a)
-{
-    //Printando opções p/ user e lendo o input
-    int op;
-    printf("Escolha uma das operações abaixo:\n1. Soma\n2. Subtração\n3. Divisão\n4. Multiplicação\n");
-    printf("Insira o número correspondente à operação desejada: ");
-    scanf("%d", &op);
-    //Se o input for inválido
-    if (op != 1 && op != 2 && op != 3 && op != 4)
-    {
-        printf("Operação inválida. Insira apenas números de 1 a 4.\n\n");
-    }
-    else
-    {
-        printf("aeee\n");
-    }
-    
-    
-}
-
 int main()
 {
-    menu(1);
+    int op;
+    do
+    {
+        //Printando opções p/ user e lendo o input   
+        printf("\n\n");
+        printf("Escolha uma das operações abaixo:\n1. Soma\n2. Subtração\n3. Divisão\n4. Multiplicação\n\n");
+        printf("Insira o número correspondente à operação desejada: ");
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 1:
+            printf("aeee\n");
+            break;
+        case 2:
+            printf("aeee\n");
+            break;
+        case 3:
+            printf("aeee\n");
+            break;
+        case 4:
+            printf("aeee\n");
+            break;
+    
+        default:
+            //Se a operação for inválida
+            //Entrando em loop infinito quando op =! int. Verificar.
+            printf("\nOperação inválida. Insira apenas números inteiros de 1 a 4.\n");
+            break;
+        }
 
+    } while (op != 1 && op != 2 && op != 3 && op != 4);
+    
+    
+    //Se o input for inválido
+    
     return 0;
 }
 
